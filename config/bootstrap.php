3b5e02f46a3c3b2bf4f8b3ec836f3a203bc6df8c
Bruno Schmidt <bruno@masterstudioweb.com.br>
2015-05-27 11:41:18 -0300

    - adicionado configuração de cache

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
 use AppCore\Lib\PluginStarter;
+use Cake\Cache\Cache;
 
 $starter = new PluginStarter();
 $starter->load('PhotoGallery');
+
+Cache::config('photo_gallery_cache', [
+    'className' => 'Cake\Cache\Engine\FileEngine',
+    'duration' => '+1 week',
+    'probability' => 100,
+    'path' => CACHE . 'plugins' . DS . 'photo_gallery' . DS,
+]);
