88e1b3a18430296ba5c5c99cd6942bbb7ea8456c
Bruno Schmidt <bruno@masterstudioweb.com.br>
2015-11-05 16:46:22 -0200

    fix: AppController estava usando a controller base errada

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,14 +2,14 @@
 
 namespace PhotoGallery\Controller;
 
-use App\Controller\AppController as BaseController;
+use ControlPanel\Controller\AppController as BaseController;
 use Cake\Core\Configure;
 
 class AppController extends BaseController
 {
 	/**
 	 * Método responsável pela inicialização e atribuição de alguns valores após o bootstrap para o correto funcionamento do plugin.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function initialize(){
