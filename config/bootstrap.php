8018c834bb3000828c2b6defcf38d17cea2cf056
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2016-02-23 15:52:58 -0300

    corrigido a forma como o arquivo de configuração do plugin é carregado

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,24 @@
 <?php
 
 use Cake\Core\Configure;
+use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Cache\Cache;
 
-if(file_exists(CONFIG . '/photo_gallery.php'))
-  Configure::load(CONFIG . '/photo_gallery.php');
-else
-  Configure::load(dirname(__FILE__) . '/default_settings.php');
+$configDir = dirname(__FILE__) . DS;
+
+try {
+  if(file_exists(CONFIG . '/photo_gallery.php')) {
+    Configure::load('photo_gallery', 'default', false);
+  }
+  else {
+    Configure::config('photo_gallery_config', new PhpConfig($configDir));
+    Configure::load('default_settings', 'photo_gallery_config', false);
+    Configure::drop('photo_gallery_config');
+  }
+}
+catch(\Exception $e) {
+  die($e->getMessage());
+}
 
 Cache::config('photo_gallery_cache', [
     'className' => 'Cake\Cache\Engine\FileEngine',
